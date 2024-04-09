0be4e1a42e61cba355c08e00bcfb8c54c522a67c
Li <varlamova_e.t@mail.ru>
2024-04-09 16:47:23 +0300

    0904

---
+++ src/main/java/ru/gb/diplom/entity/Oil.java
@@ -0,0 +1,10 @@
+package ru.gb.diplom.entity;
+
+import lombok.Data;
+
+import java.util.UUID;
+@Data
+public class Oil {
+    private UUID id;
+    private String name;
+}
---
+++ src/main/java/ru/gb/diplom/repository/OilRepository.java
@@ -0,0 +1,6 @@
+package ru.gb.diplom.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OilRepository extends JpaRepository {
+}
